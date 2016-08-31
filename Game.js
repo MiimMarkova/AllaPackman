fcb5ba8bc690465d9adc3120ef361fce4e6652d1
MiimMarkova <mariapmarkova@abv.bg>
2016-08-31 09:37:05 +0300

    Add files via upload

--- Game.js
+++ Game.js
@@ -4,24 +4,25 @@
 
 function Game () {
 	
-	var player = new Player();
 	var context = document.getElementById('canvas').getContext('2d');
+	var player = new Player(100, 100, 50, 'black', context);
 	
 	this.getPlayer = function () {
-		returnplayer;
+		return player;
 	}
 	
 	function loop() {
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		player.init();
+		player.move();
 		player.draw();
 		
 	requestAnimationFrame(loop);
 	}
+	
 	requestAnimationFrame(loop);
 }
 
-Game.prototype.init(){
+Game.prototype.init = function (){
 	this.getPlayer().draw();
-	
+	this.getPlayer().init();
 }
--- PlayerBall.js
+++ PlayerBall.js
@@ -9,7 +9,7 @@ function Player (x, y, size,  color, cxt) {
 	var lives = 3;
 	var points = 0;
 	var speed = 3;
-	 this.moveLeft = false;
+	this.moveLeft = false;
 	 this.moveRight = false;
 	 this.moveUp = false;
 	 this.moveDown = false;
@@ -38,33 +38,101 @@ function Player (x, y, size,  color, cxt) {
 		speed = newSpeed;
 	}
 	
+	this.move = function () {
+		if  (this.moveUp) {
+			this.y -= this.speed;
+		}
+		
+		if  (this.moveDown) {
+			this.y += this.speed;
+		}
+		
+		if  (this.moveLeft) {
+			this.x -= this.speed;
+		}
+		
+		if  (this.moveRight) {
+			this.x += this.speed;
+		}
+	}
+	
+	this.handleKey = function (key, value) {
+		if (key == 37) {
+			this.moveLeft = value;
+		}
+		
+		if (key == 38) {
+			this.moveUp = value;
+		}
+		
+		if (key == 39) {
+			this.moveRight = value;
+		}
+		
+		if (key == 40) {
+			this.moveDown = value;
+		}
+	}
+	this.init = function () {
+			
+			document.addEventListener('keyup', function(e){
+				this.handleKey(e.keyCode, false);
+			});
+			
+			document.addEventListener('keydown', function(e){
+				this.handleKey(e.keyCode, true);
+			});
+	
+	}
+	/*this.move = function () {
+		
+		if (this.moveLeft) {
+			console.log('left')
+			cxt.clearRect(0, 0, canvas.width, canvas.height);
+			this.setX(this.getX() - this.getSpeed());
+			this.draw();
+		}
+		
+		if (this.moveRight) {
+			this.setX(this.getX() + this.getSpeed());
+		}
 
+		if (this.moveUp) {
+			this.setY(this.getY() - this.getSpeed());
+		}
+
+		if (this.moveDown) {
+			this.setY(this.getY() - this.getSpeed());
+		}
+		
+	}
+	*/
+	
+	/*this.handleKeyUp = function () {
+		document.addEventListener('keyup', function(e) {
+			if (e.keyCode == 37) {
+				this.moveLeft = false;
+				console.log('stop');
+			}
+			if (e.keyCode == 38) {
+				this.moveUp = false;
+			}
+			
+			if (e.keyCode == 39) {
+				this.moveRight = false;
+			}
+			
+			if (e.keyCode == 40) {
+				this.moveDown = false;
+			}
+		}, false);
+		
+	}*/
 	
 }
 
 Player.prototype = Object.create(Ball.prototype);
 Player.prototype.constructor = Player;
-
-Player.prototype.init = function () {
-
-	document.addEventListener('keydown', function (e) {
-		if (e.keycode == 37) {
-			this.moveLeft();
-		}
-		if (e.keycode == 38) {
-			this.moveUp();
-		}
-
-		if (e.keycode == 39) {
-			this.moveRight();
-		}
-
-		if (e.keycode == 40) {
-			this.moveDown();
-		}
-	}, false);
-
-}
 Player.prototype.draw = function () {
 	
 	var cxt = this.getContext();
@@ -75,6 +143,13 @@ Player.prototype.draw = function () {
 	cxt.arc(this.getX(), this.getY(), this.getRadius(), 0, Math.PI*2, true);
 	cxt.fill();
 	cxt.closePath();
+}
+Player.prototype.init = function () {
+	console.log('init');
+	this.handleKey();
+	
+	//this.handleKeyUp();
+	
 }
 /*Player.prototype.move = function {
 	if (this.moveLeft) {
@@ -96,11 +171,13 @@ Player.prototype.draw = function () {
 
 */
 
-Player.prototype.moveLeft = function () {
+/*Player.prototype.moveLeft = function () {
+	console.log('func');
 	this.setX(this.getX() - this.getSpeed());
 }
 Player.prototype.moveRight = function () {
-	
+	console.log('func');
+
 	this.setX(this.getX() + this.getSpeed());
 }
 Player.prototype.moveUp = function () {
@@ -114,6 +191,27 @@ Player.prototype.moveDown = function () {
 	this.setY(this.getY() - this.getSpeed());
 }
 
+Player.prototype.handleKey = function () {
+	console.log('in');
+	document.addEventListener('keydown', function(e) {
+		console.log('fuck you')
+		if (e.keyCode == '37') {
+			Player.moveLeft() ;
+			console.log('left');
+		}
+		if (e.keyCode == 38) {
+			this.moveUp() ;
+		}
+		
+		if (e.keyCode == 39) {
+			this.moveRight();
+		}
+		
+		if (e.keyCode == 40) {
+			this.moveDown();
+		}
+	}, false);
+}*/
 Player.prototype.takeBonus = function () {
 	
 }
